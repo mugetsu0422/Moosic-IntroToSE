76ddfef8bbdb811fe65f362ba568707ee2e7a41c
phucthm <106515314+phucthm@users.noreply.github.com>
2022-06-23 10:12:34 +0700

    95% Profile

--- src/app/screens/Profile.js
+++ src/app/screens/Profile.js
@@ -1,8 +1,15 @@
 import * as React from 'react';
-import { Platform ,Text, View, StyleSheet, Image} from "react-native";
-import { CheckBox } from "react-native-elements";
+import {
+  Platform,
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+  Alert
+} from 'react-native';
+import { CheckBox } from 'react-native-elements';
 import Constants from 'expo-constants';
-
 
 // You can import from local files
 //import AssetExample from './components/AssetExample';
@@ -11,49 +18,111 @@ import Constants from 'expo-constants';
 import { Card } from 'react-native-paper';
 
 export default function App() {
-  const [male,setMale] = React.useState(false);
-  const [female,setfeMale] = React.useState(false);
+   
+
+   
+  const [male, setMale] = React.useState(false);
+  const [female, setfeMale] = React.useState(false);
   return (
     <View style={styles.container}>
-      <View style ={styles.des} />
+      <View style={styles.des} />
       <View style={styles.info}>
         <View style={styles.avatar1}>
-          <Image style={styles.avatar} source={require('./assets/Avatar.png')} />
-          <View style = {styles.box1}>
-            <Text style = {{marginVertical: 6, fontSize: 20}}> Nickname </Text>
+          <Image
+            style={styles.avatar}
+            source={require('./assets/Avatar.png')}
+          />
+
+          <View>
+            <TextInput
+              style={styles.box1}
+              placeholder="NickName"
+              placeholderTextColor="gray"
+            />
           </View>
-          <View style={styles.box2}>
-            <Text style = {{marginVertical: 6, fontSize: 20}} 
-            onPress={() => text => console.log(text)}
-              > 
-              First Name </Text>
-            
+
+          <View >
+            <TextInput
+              style={styles.box2}
+              placeholder="First Name"
+              placeholderTextColor="gray"
+            />
           </View>
-          <View style={styles.box3}>
-            <Text style = {{marginVertical: 6, fontSize: 20}}> Last Name </Text>
+
+          <View >
+              <TextInput
+                style={styles.box3}
+                placeholder="Last Name"
+                placeholderTextColor="gray"
+              />
           </View>
-          <View style={styles.box4}>
-            <Text style = {{marginVertical: 6, fontSize: 20}}> Email </Text>
+
+
+          <View>
+              <TextInput
+                style={styles.box4}
+                placeholder="Email"
+                placeholderTextColor="gray"
+              />
           </View>
-          <View style={styles.box4}>
-            <Text style = {{marginVertical: 6, fontSize: 20}}> Birthday </Text>
+
+          <View >
+              <TextInput
+                style={styles.box4}
+                placeholder="Birthday"
+                placeholderTextColor="gray"
+              />
           </View>
+
           <View style={styles.gender}>
-            <Text style = {{marginVertical: 6, fontSize: 20}}> Gender: </Text>
-            <CheckBox containerStyle={{backgroundColor: '#faedef', borderColor:'#faedef', height:45, width: 90, marginLeft:90, marginTop: -38 }}  title='Male' checked={male} onPress={() => (setMale(!male), setfeMale(false))}/>
-            <CheckBox containerStyle={{backgroundColor: '#faedef', borderColor:'#faedef',height:45, width: 90, marginLeft:200, marginTop: -49 }} title='Female' checked={female} onPress={() => (setfeMale(!female), setMale(false))}/>
+            <Text style={{ marginVertical: 6, fontSize: 20 }}> Gender: </Text>
+            <CheckBox
+              containerStyle={{
+                backgroundColor: '#faedef',
+                borderColor: '#faedef',
+                height: 45,
+                width: 90,
+                marginLeft: 90,
+                marginTop: -38,
+              }}
+              title="Male"
+              checked={male}
+              onPress={() => (setMale(!male), setfeMale(false))}
+            />
+            <CheckBox
+              containerStyle={{
+                backgroundColor: '#faedef',
+                borderColor: '#faedef',
+                height: 45,
+                width: 90,
+                marginLeft: 200,
+                marginTop: -49,
+              }}
+              title="Female"
+              checked={female}
+              onPress={() => (setfeMale(!female), setMale(false))}
+            />
           </View>
-          <View style={styles.box4}>
-            <Text style = {{marginVertical: 6, fontSize: 20}}> Phone number </Text>
+
+          <View>
+              <TextInput
+                style={styles.box4}
+                placeholder="Phone number"
+                placeholderTextColor="gray"
+              />
           </View>
-          <View style={styles.button}> 
+
+          <View style={styles.button}>
             <View style={styles.button1}>
-              <Text style={styles.textBut}> Change profile </Text>
+              <Text style={styles.textBut}   onPress={() => Alert.alert("Successful", "You have successfully changed profile")} > Change profile </Text>
+              
+
             </View>
-          </View>
+          </View> 
+
         </View>
       </View>
-      <View style ={styles.des} />
+      <View style={styles.des} />
     </View>
   );
 }
@@ -67,18 +136,19 @@ const styles = StyleSheet.create({
 
   des: {
     flex: 1.1,
-    backgroundColor: '#de4552'
+    backgroundColor: '#de4552',
   },
   info: {
     flex: 8,
     backgroundColor: '#faedef',
   },
   avatar1: {
-    flex:1,
+    flex: 1,
+    marginTop: 10,
   },
   avatar: {
-    height: 120, 
-    width: 120, 
+    height: 120,
+    width: 120,
     borderRadius: 90,
     marginTop: 20,
     marginLeft: 20,
@@ -92,51 +162,63 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginTop: -70,
     marginRight: 20,
+    fontSize: 18,
+    height: 35,
+    paddingLeft: 10,
   },
 
-  box2:{
+  box2: {
     marginLeft: 20,
     borderWidth: 0.7,
     borderRadius: 5,
-    marginTop: 70,
+    marginTop: 30,
     width: 162,
+    fontSize: 18,
+    height: 35,
+    paddingLeft: 10,
   },
-  box3:{
+  box3: {
     marginLeft: 191,
     borderWidth: 0.7,
     borderRadius: 5,
-    marginTop: -37,
+    marginTop: -35,
     marginRight: 20,
     width: 162,
+    fontSize: 18,
+    height: 35,
+    paddingLeft: 10,
   },
-  box4:{
+  box4: {
     marginLeft: 20,
     borderWidth: 0.7,
     borderRadius: 5,
     marginTop: 10,
     marginRight: 20,
+    fontSize: 18,
+    height: 35,
+    paddingLeft: 10,
   },
-  button:{
+  button: {
     flex: 1,
     marginHorizontal: 100,
     marginVertical: 40,
-    },
-  button1:{
+  },
+  button1: {
     backgroundColor: '#de4552',
     height: 40,
     width: 180,
     borderRadius: 10,
   },
-  textBut:{
+  textBut: {
     color: 'white',
     marginHorizontal: 22.5,
     marginVertical: 7,
     fontSize: 20,
   },
-  gender:{
+  gender: {
     marginLeft: 20,
     marginTop: 10,
     marginRight: 20,
+    marginBottom: -10,
   },
-
 });
