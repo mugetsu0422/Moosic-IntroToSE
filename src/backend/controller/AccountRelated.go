d85bd181fb6efc9dc2b104f819bf4f50ecbb2383
Tien <73010955+tientran505@users.noreply.github.com>
2022-07-16 14:10:23 +0700

    Login API (interacted with DB) + RegisterAPI

--- src/backend/controller/AccountRelated.go
+++ src/backend/controller/AccountRelated.go
@@ -2,24 +2,42 @@ package controllers
 
 import (
 	"net/http"
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/tientran505/musicapp/db"
-	"github.com/tientran505/musicapp/model"
+	"golang.org/x/crypto/bcrypt"
+	models "github.com/tientran505/musicapp/model"
 )
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+} 
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
 
 func LoginController(c echo.Context) error {
 	user := new(models.User)
-	
+	db := db.GetDBInstance()
+
 	if err := c.Bind(user); err != nil {
 		return err
 	}
 
-	if user.Username != "admin" || user.Password != "123456" {
-		return c.JSON(http.StatusInternalServerError, "invalid password")
+	var existedUser models.User
+
+	record := db.Where("username = ?", user.Username).First(&existedUser)
+	
+	if (record.RowsAffected) == 1 {
+		if (CheckPasswordHash(user.Password, existedUser.Password)) {
+			return c.JSON(http.StatusOK, "Login successfully")
+		}	
 	}
-
-
-	return c.JSON(http.StatusOK, user)
+	
+	return c.JSON(http.StatusInternalServerError, "invalid username/password")
 }
 
 func ForgetPassword(c echo.Context) error {
@@ -27,7 +45,7 @@ func ForgetPassword(c echo.Context) error {
 	user := &models.User{}
 
 	if err := c.Bind(user); err != nil {
-		return err;
+		return err
 	}
 
 	db.Where("username = ? AND email = ?", user.Username, user.Email).Take(&user)
@@ -43,17 +61,24 @@ func RegisterController(c echo.Context) error {
 		return err
 	}
 
-	if (user.Username == "" || user.Password == "") {
+	if user.Username == "" || user.Password == "" {
 		return c.JSON(http.StatusInternalServerError, "Please add all fields")
 	}
 
 	// check if user exists
-	record := db.Where("username = ?", user.Username).Take(&user)
+	record := db.Where("username = ?", user.Username).First(&user)
 
-	if (record.RowsAffected == 1) {
+	if record.RowsAffected == 1 {
 		return c.JSON(http.StatusInternalServerError, "User already exists")
 	}
 
-	db.Create(&user)
-	return c.JSON(http.StatusOK, user)
+	hashPassword, _ := HashPassword(user.Password)
+
+	new_user := &models.User{
+		Username: user.Username,
+		Password: hashPassword,
+	}
+
+	db.Create(&new_user)
+	return c.JSON(http.StatusOK, new_user)
 }
