0c94618a995e08e12855ca474fea340e44871316
phucthm <106515314+phucthm@users.noreply.github.com>
2022-08-03 19:24:18 +0700

    profile fix and update

--- src/app/screens/Profile.js
+++ src/app/screens/Profile.js
@@ -6,126 +6,106 @@ import {
   StyleSheet,
   Image,
   TextInput,
-  Alert
+  Alert, 
+  ScrollView,
+  SafeAreaView
 } from 'react-native';
 import { CheckBox } from 'react-native-elements';
-import Constants from 'expo-constants';
 
-// You can import from local files
-//import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-
-export default function App() {
-   
-
-   
+const Profile = () =>  {
   const [male, setMale] = React.useState(false);
   const [female, setfeMale] = React.useState(false);
   return (
-    <View style={styles.container}>
+    
+    < SafeAreaView style={styles.container}>
       <View style={styles.des} />
-      <View style={styles.info}>
-        <View style={styles.avatar1}>
-          <Image
-            style={styles.avatar}
-            source={require('./assets/Avatar.png')}
-          />
-
-          <View>
-            <TextInput
-              style={styles.box1}
-              placeholder="NickName"
-              placeholderTextColor="gray"
+      < ScrollView>
+        <View style={styles.info}>
+          <View style={styles.avatar1}>
+            <Image
+              style={styles.avatar}
+              source={require('./assets/avatar.jpg')}
             />
-          </View>
 
-          <View >
-            <TextInput
-              style={styles.box2}
-              placeholder="First Name"
-              placeholderTextColor="gray"
-            />
-          </View>
-
-          <View >
+            <View >
               <TextInput
-                style={styles.box3}
-                placeholder="Last Name"
+                style={styles.box}
+                placeholder="First Name"
                 placeholderTextColor="gray"
               />
-          </View>
-
-
-          <View>
-              <TextInput
-                style={styles.box4}
-                placeholder="Email"
-                placeholderTextColor="gray"
-              />
-          </View>
-
-          <View >
-              <TextInput
-                style={styles.box4}
-                placeholder="Birthday"
-                placeholderTextColor="gray"
-              />
-          </View>
-
-          <View style={styles.gender}>
-            <Text style={{ marginVertical: 6, fontSize: 20 }}> Gender: </Text>
-            <CheckBox
-              containerStyle={{
-                backgroundColor: '#faedef',
-                borderColor: '#faedef',
-                height: 45,
-                width: 90,
-                marginLeft: 90,
-                marginTop: -38,
-              }}
-              title="Male"
-              checked={male}
-              onPress={() => (setMale(!male), setfeMale(false))}
-            />
-            <CheckBox
-              containerStyle={{
-                backgroundColor: '#faedef',
-                borderColor: '#faedef',
-                height: 45,
-                width: 90,
-                marginLeft: 200,
-                marginTop: -49,
-              }}
-              title="Female"
-              checked={female}
-              onPress={() => (setfeMale(!female), setMale(false))}
-            />
-          </View>
-
-          <View>
-              <TextInput
-                style={styles.box4}
-                placeholder="Phone number"
-                placeholderTextColor="gray"
-              />
-          </View>
-
-          <View style={styles.button}>
-            <View style={styles.button1}>
-              <Text style={styles.textBut}   onPress={() => Alert.alert("Successful", "You have successfully changed profile")} > Change profile </Text>
-              
-
             </View>
-          </View> 
 
+            <View>
+                <TextInput
+                  style={styles.box}
+                  placeholder="Email"
+                  placeholderTextColor="gray"
+                />
+            </View>
+
+            <View >
+                <TextInput
+                  style={styles.box}
+                  placeholder="Birthday"
+                  placeholderTextColor="gray"
+                />
+            </View>
+
+            <View style={styles.gender}>
+              <Text style={{ marginVertical: 6, fontSize: 20 }}> Gender: </Text>
+              <CheckBox
+                containerStyle={{
+                  backgroundColor: '#faedef',
+                  borderColor: '#faedef',
+                  height: 45,
+                  width: 90,
+                  marginLeft: 90,
+                  marginTop: -38,
+                }}
+                title="Male"
+                checked={male}
+                onPress={() => (setMale(!male), setfeMale(false))}
+              />
+              <CheckBox
+                containerStyle={{
+                  backgroundColor: '#faedef',
+                  borderColor: '#faedef',
+                  height: 45,
+                  width: 90,
+                  marginLeft: 200,
+                  marginTop: -49,
+                }}
+                title="Female"
+                checked={female}
+                onPress={() => (setfeMale(!female), setMale(false))}
+              />
+            </View>
+
+            <View>
+                <TextInput
+                  style={styles.box}
+                  placeholder="Phone number"
+                  placeholderTextColor="gray"
+                />
+            </View>
+
+            <View style={styles.button}>
+              <View style={styles.button1}>
+                <Text style={styles.textBut}   onPress={() => Alert.alert("Successful", "You have successfully changed profile")} > Change profile </Text>
+                
+
+              </View>
+            </View> 
+
+          </View>
         </View>
-      </View>
+      </ScrollView>
       <View style={styles.des} />
-    </View>
+    </SafeAreaView>
+    
   );
 }
+export default Profile
 
 const styles = StyleSheet.create({
   container: {
@@ -150,45 +130,12 @@ const styles = StyleSheet.create({
     height: 120,
     width: 120,
     borderRadius: 90,
-    marginTop: 20,
-    marginLeft: 20,
+    marginLeft: '35%',
     borderColor: 'gray',
     borderWidth: 0.3,
   },
 
-  box1: {
-    marginLeft: 170,
-    borderWidth: 0.7,
-    borderRadius: 5,
-    marginTop: -70,
-    marginRight: 20,
-    fontSize: 18,
-    height: 35,
-    paddingLeft: 10,
-  },
-
-  box2: {
-    marginLeft: 20,
-    borderWidth: 0.7,
-    borderRadius: 5,
-    marginTop: 30,
-    width: 162,
-    fontSize: 18,
-    height: 35,
-    paddingLeft: 10,
-  },
-  box3: {
-    marginLeft: 191,
-    borderWidth: 0.7,
-    borderRadius: 5,
-    marginTop: -35,
-    marginRight: 20,
-    width: 162,
-    fontSize: 18,
-    height: 35,
-    paddingLeft: 10,
-  },
-  box4: {
+  box: {
     marginLeft: 20,
     borderWidth: 0.7,
     borderRadius: 5,
